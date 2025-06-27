9bef417c182ca47b3cb751a66d91048459375f2d
Ronja Hinterding <ronja.hinterding@kip.uni-heidelberg.de>
2025-06-27 14:13:37 +0200

    Increase fitrange for taumem to 1000 mu s
    Change-Id: I869c102cce87347131422c55b03209bc446ba8bc

--- src/py/calix/constants.py
+++ src/py/calix/constants.py
@@ -14,7 +14,7 @@ capmem_level_off_time = 20000 * pq.us
 cadc_reliable_range = base.ParameterRange(30, 210)  # LSB
 
 # Boundary values for membrane time constant fit function
-tau_mem_range = base.ParameterRange(0.1 * pq.us, 100 * pq.us)
+tau_mem_range = base.ParameterRange(0.1 * pq.us, 1000 * pq.us)
 
 # Boundary values for synaptic input time constant fit function
 tau_syn_range = base.ParameterRange(0.1 * pq.us, 100 * pq.us)
